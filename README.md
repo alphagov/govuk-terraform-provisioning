87ed661f31801976cbf1ba990f34d0e5af11bc7e
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-02-05 15:08:13 +0000

    Add initial Terraform configuration
    We've used Terraform to set up a couple of resources in AWS for
    an Elasticsearch snapshot bucket.
    
    https://govuk-infrastructure-production.signin.aws.amazon.com/console

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # GOV.UK provisioning in Terraform
 
 This repository contains the configuration to provision parts of GOV.UK.
+We're using Terraform as an experiment.
+
+## Installing Terraform
+
+```
+brew install terraform
+```
+
+## Setting up credentials
+
+Create a file called `secrets.tfvars` which looks like this:
+
+```
+aws_access_key = "akia_access_key"
+aws_secret_key = "secret_key"
+```
+
+## Doing a dry-run
+
+```
+terraform plan -var-file="secrets.tfvars" .
+```
+
+## Changing things
+
+```
+terraform apply -var-file="secrets.tfvars" .
+```
+
+## Making a graph
+
+```
+terraform graph . | dot -Tpng > graph.png
+```
---
+++ users.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "eu-west-1"
+}
+
+resource "aws_iam_group" "infrastructure_group" {
+    name = "infrastructure"
+    path = "/users/"
+}
+
+resource "aws_iam_user" "alexmuller_user" {
+    name = "alexmuller"
+}
+
+resource "aws_iam_group_membership" "infrastructure_membership" {
+    name = "infrastructure-group-membership"
+    users = [
+        "${aws_iam_user.alexmuller_user.name}",
+    ]
+    group = "${aws_iam_group.infrastructure_group.name}"
+}
+
+resource "aws_iam_policy" "admin_policy" {
+    name = "admin-policy"
+    description = "Admin policy: full access"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "admin_attachment" {
+    name = "admin_policy"
+    groups = ["${aws_iam_group.infrastructure_group.name}"]
+    policy_arn = "${aws_iam_policy.admin_policy.arn}"
+}
