1610c1dfd40f629174f10bcddb36b7aafa3ef76d
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2016-03-17 15:48:32 +0000

    Make distinct WAL-E S3 buckets
    There are three instances of PostgreSQL in the stack which will need
    backing up. This makes three distinct WAL-E buckets for each one to use.

--- projects/wal-e_backups_api-postgresql/resources/storage_and_access.tf
+++ projects/wal-e_backups_api-postgresql/resources/storage_and_access.tf
@@ -1,8 +1,8 @@
 module "paired_user" {
     source = "../../../modules/paired_user"
 
-    bucket_name = "govuk-wale-backups"
+    bucket_name = "govuk-wal-e_backups_api-postgresql"
     environment = "${var.environment}"
     team        = "Infrastructure"
-    username    = "govuk-wale-backups"
+    username    = "govuk-wal-e_backups_api-postgresql"
 }
