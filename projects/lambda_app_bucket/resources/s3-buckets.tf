24ef1ce9648ed28a53d2c4fbbc899c110a1cb22f
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-05-06 14:18:37 +0100

    Create a bucket to store lambda zip files for later deployment
    We've not enabled versioning or any extra permissions yet as we
    have a further ticket to discuss and decide on that approach.

---
+++ projects/lambda_app_bucket/resources/s3-buckets.tf
@@ -0,0 +1,8 @@
+module "lambda_app_bucket" {
+    source = "../../../modules/private_s3_bucket"
+
+    bucket_name = "govuk-lambda-applications"
+    environment = "${var.environment}"
+    team        = "Infrastructure"
+    username    = "govuk-lambda-applications"
+}
