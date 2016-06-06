8bed690f36c385813d7dd93d3c67e8c83e4685fa
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-06-06 11:23:53 +0100

    wip

--- projects/lambda_app_bucket/resources/s3-buckets.tf
+++ projects/lambda_app_bucket/resources/s3-buckets.tf
@@ -1,9 +1,23 @@
-module "lambda_app_bucket" {
-    source = "../../../modules/private_s3_bucket"
+# module "lambda_app_bucket" {
+#     source = "../../../modules/private_s3_bucket"
 
-    bucket_name = "govuk-lambda-applications"
-    environment = "${var.environment}"
-    team        = "Infrastructure"
-    username    = "govuk-lambda-applications"
-    versioning  = "true"
+#     bucket_name =
+#     environment = "${var.environment}"
+#     team        = "Infrastructure"
+#     username    = "govuk-lambda-applications"
+#     versioning  = "true"
+# }
+
+resource "aws_s3_bucket" "lambda_app_bucket" {
+    acl = "public-read"
+    bucket = "govuk-lambda-applications-${var.environment}"
+
+    tags {
+        Environment = "${var.environment}"
+        Team = "Infrastructure"
+    }
+
+    versioning {
+        enabled = "true"
+    }
 }
