7d23267c8d5659d94e8019cd3eaacf1b7b233809
Laura Martin <surminus@gmail.com>
2016-06-30 11:27:28 +0100

    Temporary disable of IP restrictions

--- projects/base_aws_infrastructure/resources/ip_restriction_policies.tf
+++ projects/base_aws_infrastructure/resources/ip_restriction_policies.tf
@@ -2,29 +2,29 @@ module "admins" {
   source = "../../../modules/admins"
 }
 
-resource "aws_iam_policy" "require_office_ip" {
-  name = "require_govuk_ip_address"
-  description = "Require requests to come from one of our IPs"
-  policy = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Deny",
-    "Action": "*",
-    "Resource": "*",
-    "Condition": {"NotIpAddress": {"aws:SourceIp": [
-      ${join(",", formatlist("\"%s\"", split(",", var.office_cidrs)))},
-      ${join(",", formatlist("\"%s\"", split(",", var.environment_cidrs)))}
-    ]}}
-  }
-}
-EOT
-}
-
-resource "aws_iam_policy_attachment" "require_office_ip" {
-  name = "require_govuk_ip_address"
-  groups = [
-    "${module.admins.group_name}",
-  ]
-  policy_arn = "${aws_iam_policy.require_office_ip.arn}"
-}
+#resource "aws_iam_policy" "require_office_ip" {
+#  name = "require_govuk_ip_address"
+#  description = "Require requests to come from one of our IPs"
+#  policy = <<EOT
+#{
+#  "Version": "2012-10-17",
+#  "Statement": {
+#    "Effect": "Deny",
+#    "Action": "*",
+#    "Resource": "*",
+#    "Condition": {"NotIpAddress": {"aws:SourceIp": [
+#      ${join(",", formatlist("\"%s\"", split(",", var.office_cidrs)))},
+#      ${join(",", formatlist("\"%s\"", split(",", var.environment_cidrs)))}
+#    ]}}
+#  }
+#}
+#EOT
+#}
+#
+#resource "aws_iam_policy_attachment" "require_office_ip" {
+#  name = "require_govuk_ip_address"
+#  groups = [
+#    "${module.admins.group_name}",
+#  ]
+#  policy_arn = "${aws_iam_policy.require_office_ip.arn}"
+#}
