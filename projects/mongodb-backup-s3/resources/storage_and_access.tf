1c0ccec01359d67baa8d14cdd8ac88a5174639ea
Mikael <mikellallison@gmail.com>
2016-07-08 10:38:53 +0100

    Some slight refactoring so that we're using the private module policy for our mongo backups in S3.
    Added a bucket for mongo daily backups.

--- projects/mongodb-backup-s3/resources/storage_and_access.tf
+++ projects/mongodb-backup-s3/resources/storage_and_access.tf
@@ -1,8 +1,18 @@
-module "paired_user" {
-    source = "../../../modules/paired_user"
+module "mongodb_s3_backup" {
+    source = "../../../modules/private_s3_bucket"
 
     bucket_name = "govuk-mongodb-backup-s3"
     environment = "${var.environment}"
     team        = "Infrastructure"
     username    = "govuk-mongodb-backup-s3"
 }
+
+module "mongodb_s3_daily_backpup" {
+    source = "../../../modules/private_s3_bucket"
+
+    bucket_name = "govuk-mongodb-daily-backup-s3"
+    environment = "${var.environment}"
+    team        = "Infrastructure"
+    username    = "govuk-mongodb-daily-backup-s3"
+}
+
