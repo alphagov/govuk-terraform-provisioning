fe794f12012b61134b7948c0094c465466cc4f98
Laura Martin <surminus@gmail.com>
2016-07-18 15:52:08 +0100

    Add lifecycle to private_s3_bucket module
    This means that a default setting can be enabled for this default
    bucket used across backups.

--- modules/private_s3_bucket/main.tf
+++ modules/private_s3_bucket/main.tf
@@ -19,6 +19,11 @@ variable "versioning" {
     default = "false"
 }
 
+variable "lifecycle" {
+    type = "string"
+    default = "false"
+}
+
 
 resource "template_file" "readwrite_policy_file" {
   template = "${file("${path.module}/templates/readwrite_policy.tpl")}"
@@ -41,6 +46,23 @@ resource "aws_s3_bucket" "bucket" {
 
     versioning {
         enabled = "${var.versioning}"
+    }
+
+    lifecycle_rule {
+        prefix = ""
+        enabled = "${var.lifecycle}"
+
+        noncurrent_version_transition {
+            days = 30
+            storage_class = "STANDARD_IA"
+        }
+        noncurrent_version_transition {
+            days = 60
+            storage_class = "GLACIER"
+        }
+        noncurrent_version_expiration {
+            days = 90
+        }
     }
 }
 
