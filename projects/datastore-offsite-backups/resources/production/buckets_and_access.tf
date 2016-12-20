2d97402212f38413a492e3d95649b441cd5a09bc
Mikael <mikellallison@gmail.com>
2016-12-20 09:56:06 +0000

    GOVUK offsite backups
    An s3 bucket which serves as the endpoint for datastore backups.
    
    Added versioning and lifecycle rules because it's generally a good idea.
    
    User; govuk-offsite-backup's keys will be generated by hand from the console.
    
    See https://trello.com/c/y3JIRupU

---
+++ projects/datastore-offsite-backups/resources/production/buckets_and_access.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "govuk-offsite-backups" {
+  bucket = "${var.bucket_name}-${var.environment}"
+
+  tags {
+    Environment = "${var.environment}"
+    Team = "${var.team}"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    prefix = ""
+    enabled = true
+
+    expiration {
+      days = 1
+    }
+
+    noncurrent_version_transition {
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+}
+
+data "template_file" "read_write_user" {
+  template = "${file("projects/datastore-offsite-backups/resources/templates/read_write_user.tpl")}"
+
+  vars {
+    bucket_name = "${var.bucket_name}"
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_iam_policy" "read_write_user" {
+  name = "${var.bucket_name}"
+  policy = "${data.template_file.read_write_user.rendered}"
+}
+
+resource "aws_iam_policy_attachment" "govuk-offsite-backups" {
+  name = "${var.bucket_name}_govuk_offsite_backups_attachment_polocy"
+  users = ["${aws_iam_user.iam_user.name}"]
+  policy_arn = "${aws_iam_policy.read_write_user.arn}"
+}
+
+resource "aws_iam_user" "iam_user" {
+  name = "${var.username}"
+}
