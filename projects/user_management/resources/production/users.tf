086bc6ae548113b0dee13d745cc09d9f5e0827ae
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-03-08 13:06:34 +0000

    Manage anafernandez account with Terraform and enable access to Production
    Enable access to Infrastructure group on all environments

--- projects/user_management/resources/production/users.tf
+++ projects/user_management/resources/production/users.tf
@@ -3,6 +3,11 @@
 #   In alphabetical order.
 #
 
+resource "aws_iam_user" "anafernandez" {
+    name = "anafernandez"
+    path = "/users/"
+}
+
 resource "aws_iam_user" "brendanbutler" {
     name = "brendanbutler"
     path = "/users/"
