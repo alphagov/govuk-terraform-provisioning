5b912124ad798bf0f556c865256bcb2fbc5691cd
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-04-05 11:45:55 +0100

    Do not manage existing anafernandez account on Integration
    My account was created manually on Integration, so this code
    would probably update the user. Do not manage the  account with
    Terraform for now, I'll sort it out when I have more time

--- projects/user_management/resources/integration/infrastructure_team.tf
+++ projects/user_management/resources/integration/infrastructure_team.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_group_membership" "infrastructure_team" {
     name = "infrastructure_team-group-membership"
     users = [
-        "${aws_iam_user.anafernandez.name}",
         "${aws_iam_user.samcook.name}",
     ]
     group = "${aws_iam_group.infrastructure_team.name}"
