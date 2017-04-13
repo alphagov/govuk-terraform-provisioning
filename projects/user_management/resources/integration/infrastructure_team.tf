5fda6d20732ac6daad19564ba36129ab46f3cb1d
Laura Martin <surminus@gmail.com>
2017-04-13 12:15:00 +0100

    Add Paul and Ana to infra team in Integration

--- projects/user_management/resources/integration/infrastructure_team.tf
+++ projects/user_management/resources/integration/infrastructure_team.tf
@@ -1,6 +1,8 @@
 resource "aws_iam_group_membership" "infrastructure_team" {
     name = "infrastructure_team-group-membership"
     users = [
+        "${aws_iam_user.anafernandez.name}",
+        "${aws_iam_user.paulbowsher.name}",
         "${aws_iam_user.samcook.name}",
     ]
     group = "${aws_iam_group.infrastructure_team.name}"
