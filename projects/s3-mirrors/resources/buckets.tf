8c0570032501f3bcad9eed70e4ba9b556dca63ed
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-04-24 10:34:07 +0100

    S3 mirrors fix policy
    Instead of adding a new statement, the Pingdom policy added in:
    https://github.com/alphagov/govuk-terraform-provisioning/pull/115
    is overwriting the Fastly one.
    
    This change combines both statements in the same policy document.

--- projects/s3-mirrors/resources/buckets.tf
+++ projects/s3-mirrors/resources/buckets.tf
@@ -21,12 +21,8 @@ resource "aws_s3_bucket" "govuk_mirror" {
   }
 }
 
-resource "aws_s3_bucket_policy" "govuk_mirror_fastly_policy" {
+resource "aws_s3_bucket_policy" "govuk_mirror_read_policy" {
     bucket = "${aws_s3_bucket.govuk_mirror.id}"
-    policy = "${data.aws_iam_policy_document.s3_mirror_fastly_read_policy_doc.json}"
+    policy = "${data.aws_iam_policy_document.s3_mirror_read_policy_doc.json}"
 }
 
-resource "aws_s3_bucket_policy" "govuk_mirror_pingdom_policy" {
-    bucket = "${aws_s3_bucket.govuk_mirror.id}"
-    policy = "${data.aws_iam_policy_document.s3_mirror_pingdom_read_policy_doc.json}"
-}
