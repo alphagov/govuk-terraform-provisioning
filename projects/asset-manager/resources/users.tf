09e39f5e64f1ed0843ce6dbb49ab55f33ca3d4ea
James Mead <james@floehopper.org>
2017-10-22 16:58:25 +0100

    Extract Asset Manager policy & its document into its own file
    I'm about to add more policies to this project related to cross-region
    replication and I think breaking the files up a bit will make it easier
    to see what's changed.

--- projects/asset-manager/resources/users.tf
+++ projects/asset-manager/resources/users.tf
@@ -2,34 +2,6 @@ resource "aws_iam_user" "asset-manager" {
   name = "${var.bucket_name}-${var.environment}-user"
 }
 
-data "aws_iam_policy_document" "asset-manager" {
-  statement {
-    sid = "1"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-    resources = [
-      "arn:aws:s3:::${var.bucket_name}-${var.environment}/*"
-    ]
-  }
-
-  statement {
-    actions = [
-      "s3:ListBucket"
-    ]
-    resources = [
-      "arn:aws:s3:::${var.bucket_name}-${var.environment}"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "asset-manager" {
-  name = "asset-manager-user-iam-policy"
-  path = "/"
-  policy = "${data.aws_iam_policy_document.asset-manager.json}"
-}
-
 resource "aws_iam_policy_attachment" "asset-manager" {
   name = "asset-manager-user-iam-policy-attachment"
   users = ["${aws_iam_user.asset-manager.name}"]
