7ceddaa345f1ed6b9a55e9f9bfbe6fc8b81bd25f
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-12-13 10:27:52 +0000

    Remove the password policy management.
    This is now implemented in https://github.com/alphagov/govuk-aws/pull/413
    in the new, supported, repo.

--- old-projects/base_aws_infrastructure/resources/iam-password-policy.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length = 12
-  require_lowercase_characters = true
-  require_numbers = true
-  allow_users_to_change_password = true
-}
