46b29e5f868dae314a040abc7a083351f138ad67
Laura Martin <surminus@gmail.com>
2018-01-25 11:06:47 +0000

    Remove user project
    The way of managing users in this method has been deprecated by using
    the central GDS account and assuming roles.
    
    This repository should be deprecated where possible.

--- projects/user_management/resources/integration/ci.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_iam_group_membership" "ci" {
-    name = "ci-group-membership"
-    users = [ 
-        "${aws_iam_user.govuk-ci.name}",
-    ]
-    group = "${aws_iam_group.ci.name}"
-}
--- projects/user_management/resources/integration/training_platform.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_iam_group_membership" "training_platform" {
-    name = "training_platform-group-membership"
-    users = [ 
-        "${aws_iam_user.grahampengelly.name}",
-        "${aws_iam_user.rubenarakelyan.name}"
-    ]
-    group = "${aws_iam_group.training_platform.name}"
-}
