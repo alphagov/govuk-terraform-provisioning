ccaf35d53eadb119623da73ca5f2e034a94f2c40
Isabell (Issy) Long <isabell@issyl0.co.uk>
2018-07-09 17:32:11 +0100

    Remove the user_management project in favour of gds-users assume-role
    - I've run a `terraform destroy` in all three environments.

--- projects/user_management/resources/integration/infrastructure_team.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_group_membership" "infrastructure_team" {
-    name = "infrastructure_team-group-membership"
-    users = [
-        "${aws_iam_user.isabelllong.name}",
-        "${aws_iam_user.paulbowsher.name}",
-        "${aws_iam_user.stephenharker.name}",
-    ]
-    group = "${aws_iam_group.infrastructure_team.name}"
-}
--- projects/user_management/resources/integration/publishing-platform.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_iam_group_membership" "publishing_platform" {
-    name = "publishing_platform-group-membership"
-    users = [ 
-        "${aws_iam_user.danielroseman.name}",
-        "${aws_iam_user.kevindew.name}"
-    ]
-    group = "${aws_iam_group.publishing_platform.name}"
-}
