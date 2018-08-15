8f26ae005fc25bd8e1a6d17a3154cfa39c5c4b7b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-08-15 13:58:40 +0100

    Allow asset-manager to delete objects
    This allows new Rake tasks which make it easier to delete assets off S3.

--- projects/asset-manager/resources/policies.tf
+++ projects/asset-manager/resources/policies.tf
@@ -3,7 +3,8 @@ data "aws_iam_policy_document" "asset-manager" {
     sid = "1"
     actions = [
       "s3:GetObject",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:DeleteObject"
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_name}-${var.environment}/*"
