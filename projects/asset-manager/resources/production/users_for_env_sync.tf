38f1ea40e17ce6d81c5e0803390507f4bc6fedc0
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-01-10 16:51:50 +0000

    Remove redundant asset-manager project
    This has now been superseded by infra-assets in govuk-aws.

--- projects/asset-manager/resources/production/users_for_env_sync.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user" "asset-manager-env-sync" {
-  name = "${var.bucket_name}-${var.environment}-env-sync-user"
-}
-
-resource "aws_iam_policy_attachment" "asset-manager-env-sync" {
-  name = "asset-manager-env-sync-iam-policy-attachment"
-  users = ["${aws_iam_user.asset-manager-env-sync.name}"]
-  policy_arn = "${aws_iam_policy.asset-manager-env-sync-user.arn}"
-}
--- projects/asset-manager/resources/users.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user" "asset-manager" {
-  name = "${var.bucket_name}-${var.environment}-user"
-}
-
-resource "aws_iam_policy_attachment" "asset-manager" {
-  name = "asset-manager-user-iam-policy-attachment"
-  users = ["${aws_iam_user.asset-manager.name}"]
-  policy_arn = "${aws_iam_policy.asset-manager.arn}"
-}
